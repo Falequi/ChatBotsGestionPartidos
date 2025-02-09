7a29b3e6b13100920c11ec87ae301cc364ae9e43
Fabio Alejandro Quintero <fabio.quintero@gmail.com>
2025-02-09 08:34:00 -0500

    borramos un console

--- src/presentation/whastapp/whatsapp.controller.ts
+++ src/presentation/whastapp/whatsapp.controller.ts
@@ -9,11 +9,9 @@ export class WhatsappController {
         const message = req.body.Body; // Mensaje recibido en WhatsApp
         const from = req.body.From; // Número de WhatsApp del remitente
 
-        console.log(`📩 Mensaje recibido de ${from}: ${message}`);
-
         // Responder automáticamente con Twilio
         const twiml = new twilio.twiml.MessagingResponse();
-        twiml.message(`📢 Recibí tu mensaje: "${message}". Pronto responderé.`);
+        twiml.message(`📢 Recibí tu mensaje: Pronto responderé.`);
 
         res.type("text/xml").send(twiml.toString());
 
